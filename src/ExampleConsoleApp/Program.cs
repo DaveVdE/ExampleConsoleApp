79f5461f7decc3613037830ecf6956551252bb9e
Dave Van den Eynde <dave@groovbird.com>
2016-06-02 23:14:38 +0200

    Initial commit

---
+++ src/ExampleConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExampleLibrary;
+
+namespace ExampleConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Class1 class1;
+        }
+    }
+}
